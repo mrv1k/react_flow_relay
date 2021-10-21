95f6412aa36946e94062da1eb70f30d90164d8d7
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-10-21 13:10:48 -0400

    configure dx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+import mockImage from './assets/mock.png'
 
 function App() {
   return (
     <div className="App">
-      <header># To Do</header>
+      <div class="wip">
+        <img src={mockImage} alt="mock" className="mock-image" />
+        <span>
+          <label for="two-skills">
+            <input type="checkbox" id="two-skills" name="two-skills" />
+            See two lists of skills
+          </label>
+
+          <label for="new-skill">
+            <input type="checkbox" id="new-skill" name="new-skill" />
+            Can add new skills
+          </label>
+
+          <label for="modal-dialog">
+            <input type="checkbox" id="modal-dialog" name="modal-dialog" />
+            New skill displayed in modal dialog
+          </label>
+        </span>
+      </div>
+
+
+      <header>Knowledge base</header>
     </div>
   );
 }
