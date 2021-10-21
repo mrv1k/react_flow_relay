c43297df37d025820acd698eb2ebb28ba501e9a1
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-10-21 14:41:49 -0400

    wip: add graphql fetch without relay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,73 @@
+import { useEffect } from "react";
 import "./App.css";
-import mockImage from './assets/mock.png'
+import mockImage from "./assets/mock.png";
+import fetchGraphQL from "./relay/fetchGraphQL";
 
 function App() {
+  useEffect(() => {
+    fetchGraphQL(`#graphql
+      query {
+        frontEnd {
+          id
+          name
+          skills {
+            edges {
+              node {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    `)
+      .then((result) => {
+        console.log(result);
+      });
+  }, [])
+
   return (
     <div className="App">
-      <div class="wip">
+      <div className="wip">
         <img src={mockImage} alt="mock" className="mock-image" />
         <span>
-          <label for="two-skills">
+          <label htmlFor="two-skills">
             <input type="checkbox" id="two-skills" name="two-skills" />
             See two lists of skills
           </label>
 
-          <label for="new-skill">
+          <label htmlFor="new-skill">
             <input type="checkbox" id="new-skill" name="new-skill" />
             Can add new skills
           </label>
 
-          <label for="modal-dialog">
+          <label htmlFor="modal-dialog">
             <input type="checkbox" id="modal-dialog" name="modal-dialog" />
             New skill displayed in modal dialog
           </label>
         </span>
       </div>
 
-
       <header>Knowledge base</header>
+
+      <div style={{ display: 'flex' }}>
+        <div>
+          Frontend
+          <ul>
+            <li>js</li>
+            <li>html</li>
+            <li>css</li>
+          </ul>
+        </div>
+        <div>
+          Backend
+          <ul>
+            <li>node.js</li>
+            <li>hack</li>
+            <li>rust</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
