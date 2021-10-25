ef851bf8fa0b6ea83be32b0bddc6dbfe877fa684
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-10-25 19:24:32 -0400

    feat: SkillsList component

---
+++ frontend/src/components/SkillsList.js
@@ -0,0 +1,14 @@
+function SkillsList({ name, skills }) {
+  return (
+    <div className="frontend">
+      <h2>{name}</h2>
+      <ul className="skills">
+        {skills.map((skill) => (
+          <li key={skill.node.id}>{skill.node.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default SkillsList
