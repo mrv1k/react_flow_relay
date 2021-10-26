73da290f9478150fb472a086ab929be2b9c4dcba
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-10-25 20:11:11 -0400

    chore(deps): install eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'xo-space',
+    'plugin:react/jsx-runtime',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'relay',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+      // X flowVersion: "0.53" // Flow version
+
+    },
+  },
+  rules: {
+  },
+};
