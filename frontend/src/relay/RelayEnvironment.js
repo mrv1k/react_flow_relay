d72e9e6f360d3ab8383468ea3b5c5edf11556c2a
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2021-11-05 13:16:11 -0400

    refactor: put fetch fn and relay env in one file

--- frontend/src/relay/RelayEnvironment.js
+++ frontend/src/relay/RelayEnvironment.js
@@ -1,21 +1,29 @@
 // @flow
 
-import { Environment, Network, RecordSource, Store } from "relay-runtime";
-import fetchGraphQL from "./fetchGraphQL";
-import type { FetchFunction } from "relay-runtime/network/RelayNetworkTypes";
+import {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+  type FetchFunction,
+} from "relay-runtime";
 
-// Pass Relay "params" and (optional) variables to helper function
-const fetchRelay: FetchFunction = async (requestParams, variables) => {
-  if (requestParams?.text) {
-    return fetchGraphQL(requestParams.text, variables);
-  }
+const fetchFunction: FetchFunction = async (request, variables) => {
+  const response = await fetch("http://localhost:4000/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: request.text,
+      variables,
+    }),
+  });
 
-  return Promise.reject(Error("Request params are required but missing."));
+  return response.json();
 };
 
 // Export a singleton instance of Relay Environment configured with our network function.
 const environment: Environment = new Environment({
-  network: Network.create(fetchRelay),
+  network: Network.create(fetchFunction),
   store: new Store(new RecordSource()),
 });
 
